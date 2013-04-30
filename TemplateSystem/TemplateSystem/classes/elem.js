77d9dc14ae8f4e4c963b2eecf64d19efa043a289
Ricardo <ricardo.mello@4d.com>
2013-04-30 10:07:46 +0200

    cleaning some extra files

--- TemplateSystem/TemplateSystem/classes/elem.js
+++
@@ -1,9 +0,0 @@
-﻿var emp = model.addClass("Elem", "Elems")
-
-emp.addAttribute("ID", "storage", "long", "key auto");
-
-emp.addAttribute("alpha", "storage", "string", "btree");
-
-emp.addAttribute("mylong", "storage", "long", "btree");
-
-
